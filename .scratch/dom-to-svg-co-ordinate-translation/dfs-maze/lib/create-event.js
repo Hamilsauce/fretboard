50422eb0e53b057d1d360fdf768ed28e749f611b
hamilsauce <jacob.w.hamilton@outlook.com>
2025-04-08 11:24:50 -0500

    update

--- .scratch/dom-to-svg-co-ordinate-translation/dfs-maze/lib/create-event.js
+++
@@ -1,7 +0,0 @@
-export const createCustomEvent = (type, detail = {}) => {
-  return new CustomEvent(type, {bubbles: true, detail})
-};
-
-export const createEvent = (type, detail = {}) => {
-  return new Event(type)
-};
