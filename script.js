38438880ac06d14ddb3025e845218777995fb737
hamilsauce <jacob.w.hamilton@outlook.com>
2025-04-16 12:53:37 -0500

    update

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
 import { makeCircular, sleep } from './circular-loop-generator.js';
-// import { run } from './ScaleGenerator.js';
-// import { PitchClassSets, NoteData } from './data/index.js';
-// import { setupStrings } from '/string-view.js'
 
 const standardTuning = ['E2', 'A2', 'D3', 'G3', 'B3', 'E4']
 
-// const stuningMap = new Map(
-//   standardTuning.map((note, i) => [note, note])
-// );
-
-// const stringsNoteGenerators = standardTuning.map((baseNote, i) => run(baseNote, 'chromatic'));
-// console.warn('stringsNoteGenerators', stringsNoteGenerators)
-
-// const stringList = new Array(6)
-//   .fill([])
-//   .map((noteArray, i) => new Array(13)
-//     .fill(null)
-//     .map(() => stringsNoteGenerators[i].next().value)
-//   );
-
-// const printedMatrix = emptyMatrix.reduceRight((acc, stringArr, i) => {
-//     const stringString = stringArr
-//       .reduce((acc, note, k) => acc.concat(`\n${i} ${k}`), '')
-//     return [...acc, stringString] //.concat(stringString)
-//   }, []);
-
-// console.warn('printedMatrix', printedMatrix)
-
-let canvasEl
-let sceneEl
-let stringEls
-let lowEString
-let highEString
-let autoClickerId
-let noteTileGenerator
-let stringTileGenerators
-
+let canvasEl;
+let sceneEl;
+let stringEls;
+let lowEString;
+let highEString;
+let autoClickerId;
+let noteTileGenerator;
+let stringTileGenerators;
 
 const dispatchClick = target => {
   const ev = new PointerEvent('click', {
@@ -46,75 +20,62 @@ const dispatchClick = target => {
   target.dispatchEvent(ev);
 };
 
-const dispatchDblClick = target => {
-  const ev = new PointerEvent('dblclick', {
-    view: window,
-    bubbles: true,
-    cancelable: true
-  });
-  target.dispatchEvent(ev);
-};
-
 const randoDigi = (range = 5) => Math.floor(Math.random() * range);
 
 const autoClicker = (tileGenerators, interval = 500, clickTimes = 0) => {
+  let clickCount = 0;
+  let delay = 0;
+  let el;
   
-  let clickCount = 0
-  let delay = 0
-  let el
-  
-  const clearIntervalId = setInterval(async () => {
-    if (!autoClickerId) return
+  const autoClickerId = setInterval(async () => {
+    if (!autoClickerId) return;
     
-    const randomStringNumber = randoDigi(5)
-    
-    console.warn('randomStringNumber, clickCount', randomStringNumber, clickCount)
+    const randomStringNumber = randoDigi(5);
+
+    const result = tileGenerators[randomStringNumber].next(clickCount);
+
     if (delay) {
-      await sleep(delay)
-      delay = null
+      await sleep(delay);
+      
+      delay = null;
     }
-    const result = tileGenerators[randomStringNumber]
-      .next(clickCount)
     
-    el = result.value ?? null
+    el = result.value ?? null;
     
     if (el && !clickTimes || clickCount < clickTimes) {
-      dispatchClick(el)
-      clickCount++
+      dispatchClick(el);
+      clickCount++;
     }
     
     else if (clickCount >= clickTimes) {
-      dispatchClick(el)
+      dispatchClick(el);
       
-      clickCount = 0
-      clearInterval(clearIntervalId)
+      clickCount = 0;
+      clearInterval(autoClickerId);
     }
-    delay = randoDigi(4) * 1000
-  }, interval)
+    
+    delay = randoDigi(4) * 1000;
+  }, interval);
   
-  return clearIntervalId
+  return autoClickerId;
 };
 
 const updateAppContent = (content) => {
-  const container = document.querySelector('#app .container')
+  const container = document.querySelector('#app .container');
   container.textContent = content;
 };
 
 let stopClickId = null;
 
-const app = document.querySelector('#app')
-// const stringGens = setupStrings()
-
-// const lowEStringGen = stringGens[0]
+const app = document.querySelector('#app');
+canvasEl = document.querySelector('#canvas');
+sceneEl = document.querySelector('#scene');
 
 setTimeout(() => {
-  canvasEl = document.querySelector('#canvas');
-  sceneEl = document.querySelector('#scene');
   stringEls = sceneEl.querySelectorAll('.string-container');
   lowEString = sceneEl.querySelector('[data-base-note="E2"]');
   highEString = sceneEl.querySelector('[data-base-note="E4"]');
-  // console.warn('lowEString', lowEString)
-
+  
   stringTileGenerators = [
     makeCircular([...sceneEl.querySelector('[data-base-note="E2"]').children]),
     makeCircular([...sceneEl.querySelector('[data-base-note="A2"]').children]),
@@ -122,32 +83,16 @@ setTimeout(() => {
     makeCircular([...sceneEl.querySelector('[data-base-note="G3"]').children]),
     makeCircular([...sceneEl.querySelector('[data-base-note="B3"]').children]),
     makeCircular([...sceneEl.querySelector('[data-base-note="E4"]').children]),
-  ]
-  
-  
-  autoClickerId = autoClicker(stringTileGenerators, 400)
-  // console.warn('canvasEl', canvasEl)
-}, 250)
+  ];
+}, 250);
 
 
 
-app.addEventListener('click', async (e) => {
-  
-  // stringGens.reduce(async (acc, updateStringNote, i) => {
-  
-  // const res = lowEStringGen()
-  // const result =  lowEStringGen()
-  // console.warn('result', result)
-  // const { value, done } = await result
-  // console.warn('value', value)
-  
-  //   return updateStringNote()
-  // }, async () => null);
-  
-  if (!autoClickerId) autoClickerId = autoClicker(noteTileGenerator)
+sceneEl.addEventListener('click', async (e) => {
+  if (!autoClickerId) autoClickerId = autoClicker(stringTileGenerators);
 });
 
-app.addEventListener('dblclick', e => {
-  clearInterval(autoClickerId)
+sceneEl.addEventListener('dblclick', e => {
+  clearInterval(autoClickerId);
   autoClickerId = null;
 });
